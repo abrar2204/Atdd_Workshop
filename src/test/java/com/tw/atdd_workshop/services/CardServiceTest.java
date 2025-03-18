31097b309ce63b83f2a3529d008e01a95455a393
abrar2204 <abrar.hasan@thoughworks.com>
2025-03-18 13:23:39 +0530

    completed story 1

--- src/test/java/com/tw/atdd_workshop/services/CardServiceTest.java
+++ src/test/java/com/tw/atdd_workshop/services/CardServiceTest.java
@@ -1,8 +1,18 @@
 package com.tw.atdd_workshop.services;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.tw.atdd_workshop.domains.Card;
+import com.tw.atdd_workshop.domains.CardType;
+import com.tw.atdd_workshop.models.card.CreateCardRequest;
 import com.tw.atdd_workshop.persistence.StaticDb;
 
 public class CardServiceTest {
@@ -18,11 +28,38 @@ public class CardServiceTest {
 
     @Test
     void createCard_validRequest_createsNewCard(){
-    
+        // Arrange
+        String customerId = UUID.randomUUID().toString();
+        CreateCardRequest request = new CreateCardRequest(customerId, CardType.Visa);
+
+        // Act
+        Card newCard = service.createCard(request);
+
+        // Assert
+        List<Card> cards = StaticDb.getCards().stream().filter(c -> c.getCustomerId() == customerId).toList();
+        assertEquals(1, cards.size());
+        Card actualCard = cards.get(0);
+        assertEquals(request.cardType(), actualCard.getCardType());
+        assertEquals(newCard.getId(), actualCard.getId());
     }
 
     @Test
     void getCustomerCards_cardsExist_getsAllCustomerCard(){
-       
+        // Arrange
+        String customerId = UUID.randomUUID().toString();
+        Card card1 = new Card(UUID.randomUUID().toString(), customerId, UUID.randomUUID().toString(), CardType.Visa);
+        Card card2 = new Card(UUID.randomUUID().toString(), customerId, UUID.randomUUID().toString(), CardType.MasterCard);
+        Card card3 = new Card(UUID.randomUUID().toString(), customerId, UUID.randomUUID().toString(), CardType.RuPay);
+        Card card4 = new Card(UUID.randomUUID().toString(), UUID.randomUUID().toString(), UUID.randomUUID().toString(), CardType.RuPay);
+        Card card5 = new Card(UUID.randomUUID().toString(), UUID.randomUUID().toString(), UUID.randomUUID().toString(), CardType.RuPay);
+        StaticDb.getCards().addAll(Arrays.asList(card1, card2, card3, card4, card5));
+
+        // Act
+        List<Card> cards = service.getCustomerCards(customerId);
+
+        // Assert
+        assertEquals(3, cards.size());
+        assertTrue(cards.stream().allMatch(card -> card.getId() == card1.getId() || card.getId() == card2.getId() || card.getId() == card3.getId())); 
+        assertTrue(cards.stream().noneMatch(card -> card.getId() == card4.getId() || card.getId() == card5.getId())); 
     }
 }
--- src/test/java/com/tw/atdd_workshop/services/CustomerServiceTest.java
+++ src/test/java/com/tw/atdd_workshop/services/CustomerServiceTest.java
@@ -1,8 +1,16 @@
 package com.tw.atdd_workshop.services;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import java.util.UUID;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.tw.atdd_workshop.domains.Customer;
+import com.tw.atdd_workshop.models.customer.CreateCustomerRequest;
 import com.tw.atdd_workshop.persistence.StaticDb;
 
 public class CustomerServiceTest {
@@ -14,5 +22,51 @@ public class CustomerServiceTest {
         StaticDb.getCards().clear();
         StaticDb.getCustomers().clear();
         service = new CustomerService();
+    }
+
+
+    @Test
+    void createCustomer_validRequest_createsNewCustomer(){
+        // Arrange
+        String phoneNumber = UUID.randomUUID().toString();
+        CreateCustomerRequest request = new CreateCustomerRequest(phoneNumber);
+
+        // Act
+        Customer newCustomer = service.createCustomer(request);
+
+        // Assert
+        List<Customer> customers = StaticDb.getCustomers().stream().filter(c -> c.getPhoneNumber() == phoneNumber).toList();
+        assertEquals(1, customers.size());
+        Customer actualCustomer = customers.get(0);
+        assertEquals(request.phoneNumber(), actualCustomer.getPhoneNumber());
+        assertEquals(newCustomer.getId(), actualCustomer.getId());
+    }
+
+    @Test
+    void getCustomers_customerExist_getsCustomer(){
+        // Arrange
+        Customer expectedCustomer = new Customer(UUID.randomUUID().toString(), UUID.randomUUID().toString(), false);
+        StaticDb.getCustomers().add(expectedCustomer);
+
+        // Act
+        Customer actualCustomer = service.getCustomer(expectedCustomer.getId());
+
+        // Assert
+        assertEquals(expectedCustomer, actualCustomer);
+    }
+
+
+    @Test
+    void verifyCustomer_customerExist_verifyCustomer(){
+        // Arrange
+        Customer customer = new Customer(UUID.randomUUID().toString(), UUID.randomUUID().toString(), false);
+        StaticDb.getCustomers().add(customer);
+
+        // Act
+        service.verifyCustomer(customer.getId());
+
+        // Assert
+        Customer customerAfterVerification = service.getCustomer(customer.getId());
+        assertTrue(customerAfterVerification.getIsVerified());
     }
 }
