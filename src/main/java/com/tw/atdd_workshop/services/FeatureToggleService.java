6a0a9dcc5cef9e0c867d3f884ea31fcf1670f56f
abrar-hasan-space <abrar.hasan@space.ge>
2025-04-02 17:28:23 +0530

    Add FeatureToggleService

---
+++ src/main/java/com/tw/atdd_workshop/services/FeatureToggleService.java
@@ -0,0 +1,11 @@
+package com.tw.atdd_workshop.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FeatureToggleService {
+    
+    public Boolean IsOn(String featureName){
+        return false;
+    }
+}
